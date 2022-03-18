1074457b0241f5361a598e6d36c59d788d411a39
keffri <keffri.w.neal@gmail.com>
2022-03-18 12:24:39 -0700

    className + CSS changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import Scores from "./Scores";
 const Header = (props) => {
   return (
     <header className="header">
-      <h1 className="header__title">Memory Card Project</h1>
+      <h1 className="header__h1">Memory Card Project</h1>
       <Scores currentScore={props.currentScore} highScore={props.highScore} />
     </header>
   );
--- src/components/Scores.js
+++ src/components/Scores.js
@@ -3,10 +3,8 @@ import React from "react";
 const Scores = (props) => {
   return (
     <div className="scores">
-      <p className="scores__currentScore">
-        Current Score: {props.currentScore}
-      </p>
-      <p className="scores__highScore">High Score: {props.highScore}</p>
+      <p className="scores__p">Current Score: {props.currentScore}</p>
+      <p className="scores__p">High Score: {props.highScore}</p>
     </div>
   );
 };
