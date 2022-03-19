af25ae758cdc7ce83028c679d851b7c36c56ebee
keffri <keffri.w.neal@gmail.com>
2022-03-18 17:25:00 -0700

    adjusted code for extra cards

--- src/components/Cards/CardDisplay.js
+++ src/components/Cards/CardDisplay.js
@@ -3,6 +3,14 @@ import waterLogo from "../../images/water.png";
 import earthLogo from "../../images/earth.png";
 import fireLogo from "../../images/fire.png";
 import windLogo from "../../images/wind.png";
+import aang from "../../images/aang.png";
+import azula from "../../images/azula.png";
+import iroh from "../../images/iroh.png";
+import katara from "../../images/katara.png";
+import ozai from "../../images/ozai.png";
+import sokka from "../../images/sokka.png";
+import toph from "../../images/toph.png";
+import zuko from "../../images/zuko.png";
 import Card from "./Card";
 
 const CardDisplay = (props) => {
@@ -11,6 +19,14 @@ const CardDisplay = (props) => {
     { name: "Earth", img: earthLogo, id: "earth" },
     { name: "Fire", img: fireLogo, id: "fire" },
     { name: "Wind", img: windLogo, id: "wind" },
+    { name: "Aang", img: aang, id: "aang" },
+    { name: "Azula", img: azula, id: "azula" },
+    { name: "Iroh", img: iroh, id: "iroh" },
+    { name: "Katara", img: katara, id: "katara" },
+    { name: "Ozai", img: ozai, id: "ozai" },
+    { name: "Sokka", img: sokka, id: "sokka" },
+    { name: "Toph", img: toph, id: "toph" },
+    { name: "Zuko", img: zuko, id: "zuko" },
   ];
 
   const [currentCards, setCurrentCards] = useState([]);
@@ -24,13 +40,21 @@ const CardDisplay = (props) => {
       cardData[randomArray[1]],
       cardData[randomArray[2]],
       cardData[randomArray[3]],
+      cardData[randomArray[4]],
+      cardData[randomArray[5]],
+      cardData[randomArray[6]],
+      cardData[randomArray[7]],
+      cardData[randomArray[8]],
+      cardData[randomArray[9]],
+      cardData[randomArray[10]],
+      cardData[randomArray[11]],
     ]);
     // eslint-disable-next-line
   }, []);
 
   const randomNumberArray = () => {
     const numbers = [];
-    for (var a = [0, 1, 2, 3], i = a.length; i--; ) {
+    for (var a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], i = a.length; i--; ) {
       var random = a.splice(Math.floor(Math.random() * (i + 1)), 1)[0];
       numbers.push(random);
     }
@@ -38,7 +62,7 @@ const CardDisplay = (props) => {
   };
 
   useEffect(() => {
-    if (props.currentScore === 4) {
+    if (props.currentScore === 12) {
       alert("You win!");
       resetGame();
     }
@@ -60,6 +84,14 @@ const CardDisplay = (props) => {
       cardData[randomArray[1]],
       cardData[randomArray[2]],
       cardData[randomArray[3]],
+      cardData[randomArray[4]],
+      cardData[randomArray[5]],
+      cardData[randomArray[6]],
+      cardData[randomArray[7]],
+      cardData[randomArray[8]],
+      cardData[randomArray[9]],
+      cardData[randomArray[10]],
+      cardData[randomArray[11]],
     ]);
   };
 
@@ -78,6 +110,14 @@ const CardDisplay = (props) => {
       cardData[randomArray[1]],
       cardData[randomArray[2]],
       cardData[randomArray[3]],
+      cardData[randomArray[4]],
+      cardData[randomArray[5]],
+      cardData[randomArray[6]],
+      cardData[randomArray[7]],
+      cardData[randomArray[8]],
+      cardData[randomArray[9]],
+      cardData[randomArray[10]],
+      cardData[randomArray[11]],
     ]);
   };
 
